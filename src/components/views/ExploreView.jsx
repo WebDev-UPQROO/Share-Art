88c2e5705c9b6fbc97a4323230e39059a5749ba0
Erick Espinoza <erickesp2077@gmail.com>
2021-10-18 23:30:07 -0500

    Profile Component edited

--- src/components/views/ExploreView.jsx
+++ src/components/views/ExploreView.jsx
@@ -1,9 +1,53 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const ExploreView = () => {
     return (
         <main className="main-full">
-            <h1>ExploreView</h1>
+            <div className="explore">
+                <h1>Explorar</h1>
+                <div className="explore-categories">
+                    <div className="categories__heading">
+                        <h3>Categorias</h3>
+                        <Link to="#" className="post__link btn-animation btn-link">
+                            <span className="mr-1">Ir a categorias</span>
+                            <FontAwesomeIcon icon="arrow-right" />
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="explore-groups">
+                    <div className="groups__heading">
+                        <h3>Grupos</h3>
+                        <Link to="#" className="post__link btn-animation btn-link">
+                            <span className="mr-1">Ir al grupos</span>
+                            <FontAwesomeIcon icon="arrow-right" />
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="explore-artists">
+                    <div className="artists__heading">
+                        <h3>Artistas</h3>
+                        <Link to="#" className="post__link btn-animation btn-link">
+                            <span className="mr-1">Ir a artistas</span>
+                            <FontAwesomeIcon icon="arrow-right" />
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="explore-posts">
+                    <div className="posts__heading">
+                        <h3>Artistas</h3>
+                        <Link to="#" className="post__link btn-animation btn-link">
+                            <span className="mr-1">Ir al publicaciones</span>
+                            <FontAwesomeIcon icon="arrow-right" />
+                        </Link>
+                    </div>
+                </div>
+
+            </div>
         </main>
     )
 }

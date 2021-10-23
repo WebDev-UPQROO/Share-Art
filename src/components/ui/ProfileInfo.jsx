d932e6fca74fc730f58fec85527dec34a202fdf7
HiramOrd <hiram8012@gmail.com>
2021-10-23 16:29:00 -0500

    feat: profile design finished

--- src/components/ui/ProfileInfo.jsx
+++ src/components/ui/ProfileInfo.jsx
@@ -11,7 +11,12 @@ export const ProfileInfo = () => {
                     <img src='./assets/temp/user.jfif' alt="default" />
                 </picture>
 
-                <button className="btn btn-animation btn-outline ml-auto mb-2">Seguir</button>
+                <button
+                    className={'btn btn-animation btn-outline ml-auto mb-2' + (false && 'selected')}
+                >
+                    {(true) && <FontAwesomeIcon icon="plus" />}
+                    <span>{true ? 'Seguir' : 'Siguiendo'}</span>
+                </button>
 
                 <h2 className="profile__content__name">Nombre completo</h2>
                 <p className="profile__content__user mb-1">@usuario</p>
@@ -19,20 +24,23 @@ export const ProfileInfo = () => {
                 <p className="mb-1">aqui va a ir la descripcion de prueba de la persona, esto esta en verion alpha</p>
 
                 <p>
-                    <FontAwesomeIcon icon="user" className="mr-1"/>
+                    <FontAwesomeIcon icon="user" className="mr-1" />
                     <span>Edad: 34 años</span>
                 </p>
 
-                <p>
-                    <FontAwesomeIcon icon="drafting-compass" className="mr-1"/>
+                <p className="mt-2">
+                    <FontAwesomeIcon icon="drafting-compass" className="mr-1" />
                     <span>Intereses</span>
                 </p>
 
-                <div className="profile__content__categories mb-5">
-                    <span>Categoria 1</span>
-                    <span>Categoria 1</span>
-                    <span>Categoria 1</span>
-                    <span>Categoria 1</span>
+                <div className="profile__content__categories mb-1 mt-1">
+
+                    <div className="check">
+                        <label>
+                            <FontAwesomeIcon icon="drafting-compass" className="mr-1" />
+                            Categoria 1
+                        </label>
+                    </div>
                 </div>
             </div>
         </div>
